863a1428d3d8d3d75f521e627a5f4adeb94a1387
Eric Sabelhaus <eric.sabelhaus@gmail.com>
2015-11-07 00:37:47 -0500

    update secret key base initilization

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,5 +1,1 @@
-if Rails.env.production?
-  Rails.application.config.secret_token = ENV['SECRET_TOKEN']
-else
-  Rails.application.config.secret_token = ('x' * 30) # meets minimum requirement of 30 chars long
-end
+OpenCampaign::Application.config.secret_key_base = ENV['SECRET_TOKEN']
