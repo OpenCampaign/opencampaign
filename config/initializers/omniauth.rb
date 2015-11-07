8a9ba513c9ee3c50950081b5a24c19f83d9a2301
Steven Haddox <steven@haddox.us>
2015-11-06 23:32:08 -0500

    Gemfile.lock to latest

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,4 @@
 OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
-  unless Rails.env.production?
-    eval(IO.read(ENV['OA_DEVELOPER_PROVIDER_PATH']), binding)
-  else
-    #TODO: Extract this out to dynamically load via ENV list
-    eval(IO.read(ENV['OA_IDENTITY_PROVIDER_PATH']), binding) # omniauth-identity via BasicAuth model
-  end
+  eval(IO.read(ENV['OA_PROVIDER_PATH']), binding)
 end
