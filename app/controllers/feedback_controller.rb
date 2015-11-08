388ba2985e9062c9cbaca217d863eadef978eb88
Eric Sabelhaus <eric.sabelhaus@gmail.com>
2015-11-08 00:02:52 -0500

    feedback functionality working, still need to tidy up the views

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -11,16 +11,16 @@ class FeedbackController < ApplicationController
       subject: params[:feedback][:subject],
       first_name: params[:feedback][:first_name],
       last_name: params[:feedback][:last_name],
-      phone_number: params[:feedback][:phone_number],
       email: params[:feedback][:email],
+      phone_number: params[:feedback][:phone_number],
       address: params[:feedback][:address],
-      comment: params[:feedback][:comment],
+      comment: params[:feedback][:comment]
     )
 
     if @feedback.valid?
       @feedback.save
       redirect_to(root_path)
-      flash[:info] = "Issue Successfully Created"
+      flash[:info] = "Feedback Successfully Created"
     else
       flash[:error] = "An error occurred while creating the position"
       render :new
