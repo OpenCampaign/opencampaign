8d5926314064a44f5aa0cd4a0f075546f995aaca
Eric Sabelhaus <eric.sabelhaus@gmail.com>
2015-11-07 23:22:52 -0500

    feedback views cleaned up, almost have the controller working

--- app/controllers/admin/feedback_controller.rb
+++ app/controllers/admin/feedback_controller.rb
@@ -1,3 +1,21 @@
 class Admin::FeedbackController < AdminController
-  
+  def index
+    @feedbacks = Feedback.all
+  end
+
+  def show
+    @feedback = Feedback.find(params[:id])
+  end
+
+  def destroy
+    @feedback = Feedback.find(params[:id])
+    unless @feedback.blank?
+      @feedback.delete
+      flash[:info] = "Issue Successfully removed"
+      redirect_to(admin_feedbacks_path)
+    else
+      flash[:error] = "An error occurred while removing the feedback"
+      redirect_to(admin_feedbacks_path)
+    end
+  end
 end
--- db/migrate/20151107223447_create_feedback.rb
+++ db/migrate/20151107223447_create_feedback.rb
@@ -1,6 +1,7 @@
 class CreateFeedback < ActiveRecord::Migration
   def change
     create_table :feedbacks do |t|
+      t.string :subject, null: false
       t.string :first_name, null: false
       t.string :last_name, null: false
       t.string :email, null: false
